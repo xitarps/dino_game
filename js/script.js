b11d54766e340658f66de8c3786cbe1a70e7cb48
xita Rps <xitarps@gmail.com>
2020-11-13 02:04:10 -0300

    Add listener to space bar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-alert('js loaded!')
+//helpers
+const $ = (selector) => document.querySelector(selector);
+const log = (message) => console.log(message);
+
+//get dino div
+const dino = $('.dino');
+
+//fire when spacebar(32) get up
+const handleKeyUp = (e) => {
+  if (e.keyCode == 32){
+    log('apertou barra de espaço');
+  }
+};
+
+//add eventListener to key up
+document.addEventListener('keyup', handleKeyUp);
+
